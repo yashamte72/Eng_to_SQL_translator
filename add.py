206ed2d66bcf3a2fb01f6c37136528d9bbb8eda2
yashamte72 <yashamte123@gmail.com>
2025-04-21 13:42:13 +0530

    route for HTML file
    added

--- add.py
+++ add.py
@@ -6,3 +6,8 @@ app = Flask (__name__)
 
 #add your openai API key here
 openai.api_key = "my_key for security reasons github does not allow to push openai key" 
+
+#home Rute: serves the HTML CODES from the templates folder
+@app.route('/')
+def home():
+    return render_template('index.html')
